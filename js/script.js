483e2ee22b6b8810c7241e7689cfac998101ee36
Tyler Kuncken <126TKuncken@frhsd.com>
2025-03-17 13:58:10 -0400

    committed

--- js/script.js
+++ js/script.js
@@ -1,42 +1,46 @@
-const canvas = document. querySelector('canvas');
+const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
-canvas.width= window.innerWidth;
-canvas.height= window.innerHeight;  
 
-window.onresize = function() {
-    canvas.width = innerWidth;
-    canvas.height = innerHeight;
-}
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
-class Boundary{
-    constructor({position}){
-       this.position = position;
-       this.width = 40;
-       this.height = 40;
-}
-
+class Boundary {
+    constructor ({ position }) {
+        this.position = position;
+        this.width = 40;
+        this.height = 40;
+    }
+ 
     draw() {
         c.fillStyle = 'blue';
-    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
 }
-
-
-const map = [
-['-', '-', '-', '-', '-', '-',]
-['-', '-', '-', '-', '-', '-',]
-['-', '-', '-', '-', '-', '-',]
-['-', '-', '-', '-', '-', '-',]
+const map =[
+   ['-','-','-','-','-','-',],
+   ['-',' ',' ',' ',' ','-',],
+   ['-',' ',' ',' ',' ','-',],
+   ['-','-','-','-','-','-',]
 ]
+const boundaries = []
 
-const boundries = [];
-
-map.forEach((row,) => {
-    row.forEach((Symbol) => {
-        switch(Symbol){
+map. forEach((row, i) => {
+    row.forEach((symbol, j) => {
+        switch (symbol) {
             case '-':
-              boundries.push(
-                new Boundary({
-                    position: {x: 0, y: 40}
-                })
-              )
+                boundaries.push(
+                    new Boundary({
+                        position: {
+                            x: 40 * j,
+                            y: 40 * y
+                        }
+                    })
+                )
+                break
+        }
+    })
+})
+
+boundaries.forEach((boundary) => {
+    boundary.draw()
+})
